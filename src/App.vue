537d6c1993e4e5a32d714f11294ee27cb4f5fa32
abiassantana <abias.santana@dce.ufpb.br>
2019-04-04 16:02:41 -0300

    initial configurations

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <cadDeveloper></cadDeveloper>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import cadDeveloper from './components/cadDeveloper'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    cadDeveloper
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/cadDeveloper.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>kkkkk eae man</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'cadDev'
+}
+</script>
+
+<style>
+</style>
